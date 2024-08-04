e263441b42c6be941b45404bf3e3eb642f53d512
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-05 01:49:18 +0800

    update runtime

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
             GitHub
           </a>
         </p>
-        <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
+        <section id="footer-github" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
          © 2020-{new Date().getFullYear()}{" "} Designed by <a href="https://github.com/hamster1963">Hamster1963</a>&<a href="https://github.com/akiker233">Akiker</a>
         </section>
       </section>

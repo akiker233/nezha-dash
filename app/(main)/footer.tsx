06555ac123ccfdee1ee8b3d658942040ceaaff9f
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-05 01:46:23 +0800

    update runtime

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
        <div id="runtime" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50"></div>
-        <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
+        <p id="footer-github" className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           Find the code on{" "}
           <a
             href="https://github.com/akiker233/nezha-dash"

79fece32d8cf4a146ae13e76825de826c52be6ff
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-04 22:20:01 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import "/script/cherry.js";
+import "@/scripts/cherry.js";
 
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";

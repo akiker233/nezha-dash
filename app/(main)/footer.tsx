b7c593ba7d875d3a6cd8872daf9e231d5912656c
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-04 21:53:48 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           Find the code on{" "}
           <a
-            href="https://github.com/hamster1963/nezha-dash"
+            href="https://github.com/akiker233/nezha-dash"
             target="_blank"
             className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
           >

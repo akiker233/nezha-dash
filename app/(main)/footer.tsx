2794d0e91bf946330cd5abaf5c86c6c182079fac
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-05 00:55:59 +0800

    update runtime

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
+        <div id="runtime" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50"></div>
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           Find the code on{" "}
           <a
@@ -15,7 +16,6 @@ export default function Footer() {
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
          © 2020-{new Date().getFullYear()}{" "} Designed by <a href="https://github.com/hamster1963">Hamster1963</a>&<a href="https://github.com/akiker233">Akiker</a>
         </section>
-        <div id="runtime" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50"></div>
       </section>
     </footer>
   );

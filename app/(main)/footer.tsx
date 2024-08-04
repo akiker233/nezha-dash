3fc9abd0be481a54911428dfdf3a37041e714c60
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-05 00:52:55 +0800

    update runtime

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
          © 2020-{new Date().getFullYear()}{" "} Designed by <a href="https://github.com/hamster1963">Hamster1963</a>&<a href="https://github.com/akiker233">Akiker</a>
         </section>
-        <div id="runtime"></div>
+        <div id="runtime" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50"></div>
       </section>
     </footer>
   );

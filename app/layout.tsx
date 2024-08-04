e2f4d714cacfa69232e2bd0f1e8a61b7f3748f00
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-04 22:07:05 +0800

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import "https://fastly.jsdelivr.net/gh/mocchen/cssmeihua/js/yinghua.js";
+require('https://fastly.jsdelivr.net/gh/mocchen/cssmeihua/js/yinghua.js');
 
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";

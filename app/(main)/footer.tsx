21d6ebd3509ef27617efa9bf5e3ef36f009fb9ea
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-04 21:45:50 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
           </a>
         </p>
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
-         <p>© 2020-<!-- -->2024<!-- --> <a href="https://github.com/hamster1963">@Hamster1963</a><br>
+         <p>© 2020-{new Date().getFullYear()}{" "} <a href="https://github.com/hamster1963">@Hamster1963</a><br>
 Powered by<a href="https://github.com/akiker233"> Akiker</a></p>
         </section>
       </section>

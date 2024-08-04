e54b0177399f326d69a73665a04d84dcea922a4a
akiker233 <94284604+akiker233@users.noreply.github.com>
2024-08-05 01:38:22 +0800

    update runtime

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -2,9 +2,8 @@ export default function Footer() {
   return (
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
+       <div id="runtime" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50"></div>
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
-         <div id="runtime" className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50"></div><br />
-         <p id="footer-github" >
           Find the code on{" "}
           <a
             href="https://github.com/akiker233/nezha-dash"
@@ -13,7 +12,6 @@ export default function Footer() {
           >
             GitHub
           </a>
-          </p>
         </p>
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
          © 2020-{new Date().getFullYear()}{" "} Designed by <a href="https://github.com/hamster1963">Hamster1963</a>&<a href="https://github.com/akiker233">Akiker</a>
